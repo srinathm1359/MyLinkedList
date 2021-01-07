425a6cb65b6b83831c502fb6d3522c51754b2a5f
srinathstuycs <smahankali10@stuy.edu>
2021-01-07 13:56:39 -0500

    Created Node.java

---
+++ Node.java
@@ -0,0 +1,6 @@
+public class Node{
+  private String data;
+  private Node next,prev;
+  public Node(String value){/*create a constructor*/}
+ //write get/set methods for all three instance variables.
+}
