6e302f2263d0d1163eee59e2252b3f94540af892
srinathstuycs <smahankali10@stuy.edu>
2021-01-10 21:40:49 -0500

    Added method add(index, value)

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -37,8 +37,42 @@ public class MyLinkedList{
    end = newNode;
    return true;
  }
+
+ public void add(int index, String value) {
+   Node nodeToAdd = new Node(value);
+   //If outside range
+   if (index < 0 || index > size()) {
+     throw new IndexOutOfBoundsException("Index needs to be between 0 and " + toString(size()) + ", inclusive!");
+   }
+   //If at end
+   else if (index == size()) {
+     if (size() == 0) {
+       start = nodeToAdd;
+     }
+     else {
+       nodeToAdd.setPrev(end);
+       end.setNext(nodeToAdd);
+     }
+     end = newNode;
+   }
+   //If at start
+   else if (index == 0) {
+     start.setPrev(nodeToAdd);
+     nodeToAdd.setNext(start);
+     start = nodeToAdd;
+   }
+   //If at middle of linked list
+   else {
+     Node prev = getNodeFromIndex(index - 1);
+     Node next = prev.getNext();
+     nodeToAdd.setPrev(prev);
+     nodeToAdd.setNext(next);
+     prev.setNext(nodeToAdd);
+     next.setPrev(nodeToAdd);
+   }
+   size++;
+ }
  /*
- public void add(int index, String value);
  public String get(int index);
  public String set(int index, String value);
  public String toString();
