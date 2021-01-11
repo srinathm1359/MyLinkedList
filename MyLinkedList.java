14ae7b789ef1194b6d2eb86177fd8b4e93de0307
srinathstuycs <smahankali10@stuy.edu>
2021-01-10 19:42:40 -0500

    Created constructor

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,19 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
+ 
+ public MyLinkedList() {
+   size = 0;
+   start = null;
+   end = null;
+ }
+ /*
  public int size();
  public boolean add(String value);
  public void add(int index, String value);
  public String get(int index);
  public String set(int index, String value);
  public String toString();
+ */
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
